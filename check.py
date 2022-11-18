00873716dcf278afffdfd1a3ff6d65a3131aa12d
zacharybottorff <zachbot00@gmail.com>
2022-11-18 15:08:50 -0600

    Major improvements to check.py

--- check.py
+++ check.py
@@ -10,13 +10,32 @@ import os
 # Array of images
 a = np.load('./result/SMILE_Pink_SR5p_10.npy') #32,32
 
+# Original images
+b = np.load('./image/Smile_image.npy')
+
 # repeat for 10 images
 for i in range(10):
+  # Make blank figure
+  fig = plt.figure(figsize=(16, 8))
+  rows = 1
+  columns = 2
+  fig.add_subplot(rows, columns, 1)
   # put image i in figure
   # TODO: consider different normalization modes of imshow()
   # NOTE: may need to convert vmax to 1
-  plt.imshow(a[i], cmap='gray', vmin=0, vmax=255)
+  plt.imshow(b[i], cmap='gray', vmin=0, vmax=1)
   # turn off axis
   plt.axis('off')
+  
+  plt.title('Input')
+  
+  fig.add_subplot(rows, columns, 2)
+  
+  plt.imshow(a[i], cmap='gray', vmin=0, vmax=1)
+  
+  plt.axis('off')
+  
+  plt.title('Output')
+  
   # display open figure
   plt.show()
